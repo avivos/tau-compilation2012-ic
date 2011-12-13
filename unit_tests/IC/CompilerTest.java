9d1154ed5447a16cd9a87fb8b600307dfef328df
zzzoshri@gmail.com <zzzoshri@gmail.com@06810c4b-2e02-f893-feeb-4881b2ea26cb>
2011-12-13 21:37:27 +0000

    i'm writing a test-case for the compiler it's still not ready

---
+++ unit_tests/IC/CompilerTest.java
@@ -0,0 +1,14 @@
+package IC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompilerTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ unit_tests/IC/TestUtils.java
@@ -0,0 +1,13 @@
+package IC;
+
+public class TestUtils {
+
+	public static void executeMain(String[] args){
+		IC.Compiler.main(args);
+	}
+	
+	public static boolean checkState(){
+		FileReader reader = new fileReader("main.state");
+	}
+	
+}
