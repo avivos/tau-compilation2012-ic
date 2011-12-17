7a3b719ddcfe2ba6678ed737c358b87d765eb8a3
zzzoshri@gmail.com <zzzoshri@gmail.com@06810c4b-2e02-f893-feeb-4881b2ea26cb>
2011-12-17 12:46:14 +0000

    added a JUnit test class - tests can be run automatically to write a new test: 1. write a test file you want the compiler to run 2. save the file as .good if it is a legal input, 	and as .bad otherwise. 3. add a test method in CompilerTest.java (just copy and paste one of 	the other methods and change the name and file-name)

--- trunk/unit_tests/IC/TestUtils.java
+++ trunk/unit_tests/IC/TestUtils.java
@@ -1,13 +1,53 @@
 package IC;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TestUtils {
 
 	public static void executeMain(String[] args){
 		IC.Compiler.main(args);
 	}
-	
-	public static boolean checkState(){
-		FileReader reader = new fileReader("main.state");
+
+	public static boolean runTestFile(String testFilename, String otherArgs){
+		//set an expected boolean value to compare the test run
+		boolean expected;
+		if (testFilename.endsWith(".good")){
+			expected = true;
+		}
+		else if (testFilename.endsWith(".bad")){
+			expected = false;
+		}
+		else {
+			return false;
+		}
+		
+		
+		try {
+			// building the args String
+			StringBuffer args = new StringBuffer();
+			args.append(testFilename);
+			if (!otherArgs.equals("")){
+				args.append(" ");
+				args.append(otherArgs);
+			}
+			executeMain(args.toString().split(" "));
+			
+			byte[] buffer = new byte[(int) new File("main.state").length()];
+			BufferedInputStream f = new BufferedInputStream(new FileInputStream("main.state"));
+
+			f.read(buffer);
+			String state = new String(buffer);
+			boolean ret = (state.startsWith("OK") == expected);
+			return ret;
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
 }
