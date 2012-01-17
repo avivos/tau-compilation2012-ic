279257724412565359e0589ced4140864ad22ccf
adi.zarfaty@gmail.com <adi.zarfaty@gmail.com@06810c4b-2e02-f893-feeb-4881b2ea26cb>
2012-01-17 17:00:22 +0000

    PA3 submitted

--- trunk/unit_tests/IC/IllegalCompilerTest.java
+++ trunk/unit_tests/IC/IllegalCompilerTest.java
@@ -20,6 +20,66 @@ public class IllegalCompilerTest {
 	public void full_ast_test() {
 		assertTrue(TestUtils.runTestFile(testDir + "full_ast_test.ic.bad", "-print-ast -debug"));
 	}
+	
+	@Test
+	public void BadAccess_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "BadAccess.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void classNotDefine_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "classNotDefine.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void DoubleMain_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "DoubleMain.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void duplicatedField_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "duplicatedField.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void MethodCallForNonClass_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "MethodCallForNonClass.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void NonIntIndex_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "NonIntIndex.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void NoSuchFunc_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "NoSuchFunc.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void NoSuchStaticClass_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "NoSuchStaticClass.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void NoSuchStaticFunc_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "NoSuchStaticFunc.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void Overload_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "Overload.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void Overload2_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "Overload2.ic.bad", "-print-ast -debug"));
+	}
+	
+	@Test
+	public void wrongArguments_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "wrongArguments.ic.bad", "-print-ast -debug"));
+	}
 }
 
 
