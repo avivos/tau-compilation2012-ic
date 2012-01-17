d523645bd54d4055a926d6423b73d924319edab7
adi.zarfaty@gmail.com <adi.zarfaty@gmail.com@06810c4b-2e02-f893-feeb-4881b2ea26cb>
2012-01-17 17:00:22 +0000

    PA3 submitted

--- unit_tests/IC/LegalCompilerTest.java
+++ unit_tests/IC/LegalCompilerTest.java
@@ -8,46 +8,59 @@ public class LegalCompilerTest {
 
 	String testDir = "src/IC.test2/legal_input/";
 	// LEGAL FILES - TESTS
-	@Test
-	public void empty_program_file() {
-		assertTrue(TestUtils.runTestFile(testDir + "program_test.ic.good", "-debug"));
-	}
-	
-	@Test
-	public void class_level() {
-		assertTrue(TestUtils.runTestFile(testDir + "classDecl_test.ic.good", "-debug"));
-	}
-	
-	@Test
-	public void method_level() {
-		assertTrue(TestUtils.runTestFile(testDir + "method_test.ic.good", "-debug"));
-	}
-	
-	@Test
-	public void type_level() {
-		assertTrue(TestUtils.runTestFile(testDir + "field_test.ic.good", "-print-ast -debug"));
-	}
-	
-	@Test
-	public void method_with_statement() {
-		assertTrue(TestUtils.runTestFile(testDir + "method_with_statement.ic.good", "-debug"));
-	}
-	
-	@Test
-	public void Quicksort() {
-		assertTrue(TestUtils.runTestFile(testDir + "Quicksort.ic.good", "-debug"));
-	}
+//	@Test
+//	public void empty_program_file() {
+//		assertTrue(TestUtils.runTestFile(testDir + "program_test.ic.good", "-debug"));
+//	}
+//	
+//	@Test
+//	public void class_level() {
+//		assertTrue(TestUtils.runTestFile(testDir + "classDecl_test.ic.good", "-debug"));
+//	}
+//	
+//	@Test
+//	public void method_level() {
+//		assertTrue(TestUtils.runTestFile(testDir + "method_test.ic.good", "-debug"));
+//	}
+//	
+//	@Test
+//	public void type_level() {
+//		assertTrue(TestUtils.runTestFile(testDir + "field_test.ic.good", "-print-ast -debug"));
+//	}
+//	
+//	@Test
+//	public void method_with_statement() {
+//		assertTrue(TestUtils.runTestFile(testDir + "method_with_statement.ic.good", "-debug"));
+//	}
+//	
+//	@Test
+//	public void Quicksort() {
+//		assertTrue(TestUtils.runTestFile(testDir + "Quicksort.ic.good", "-debug"));
+//	}
+//
+//	@Test
+//	public void Quicksort_modified() {
+//		assertTrue(TestUtils.runTestFile(testDir + "Quicksort_modified.ic.good", "-debug"));
+//	}
+//	
+//	@Test
+//	public void full_ast_test_legal() {
+//		assertTrue(TestUtils.runTestFile(testDir + "full_ast_test_legal.ic.good", "-debug"));
+//	}
 
+//	@Test
+//	public void legalprog_test() {
+//		assertTrue(TestUtils.runTestFile(testDir + "legalprog.ic.good", "-debug"));
+//	}
+	
 	@Test
-	public void Quicksort_modified() {
-		assertTrue(TestUtils.runTestFile(testDir + "Quicksort_modified.ic.good", "-debug"));
+	public void legalprog2_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "legalprog2.ic.good", "-debug"));
 	}
 	
 	@Test
-	public void full_ast_test_legal() {
-		assertTrue(TestUtils.runTestFile(testDir + "full_ast_test_legal.ic.good", "-debug"));
+	public void example2_test() {
+		assertTrue(TestUtils.runTestFile(testDir + "example2.ic.good", "-debug"));
 	}
-
-	
 	
 }
